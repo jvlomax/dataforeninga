96cc0fb2000a1472bc85f3fe30de1669eb018a0e
Jørn Lomax <northlomax@gmail.com>
2014-10-11 11:32:02 +0200

    created custom 404 page

--- main.py
+++ main.py
@@ -62,5 +62,11 @@ def showAdmin():
 def showServers():
     servers = Servers.query.all()
     return render_template("servers.html", servers=servers)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
