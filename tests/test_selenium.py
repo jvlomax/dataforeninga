16130abb010c4cf50dc38892ffb582c04d4d9a05
Jørn Lomax <northlomax@gmail.com>
2014-10-14 20:55:08 +0200

    added selenium tests for the menu

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -28,11 +28,28 @@ class MyTest(TestCase):
         #driver = webdriver.Firefox()
         self.driver.get("http://127.0.0.1:5000/index.html")
         assert self.driver.title == "Tromsøstudentenes Dataforening"
+
     def test_menu(self):
         self.driver.get("http://127.0.0.1:5000/")
-       # contact_link = self.driver.find_element_by_link_text("Kontakt Oss")
+        assert self.driver.title == "Tromsøstudentenes Dataforening | Hjem"
         board_link = self.driver.find_element_by_link_text("Styret")
-       # home_link = self.driver.find_element_by_link_text("Hjem")
         board_link.click()
+        assert self.driver.title == "Tromsøstudentenes Dataforening | Styret"
+        about_link = self.driver.find_element_by_link_text("Om oss")
+        about_link.click()
+        assert self.driver.title == "Tromsøstudentenes Dataforening | Om oss"
+        contact_link = self.driver.find_element_by_link_text("Kontakt oss")
+        contact_link.click()
+        assert self.driver.title == "Tromsøstudentenes Dataforening | Kontakt oss"
+        home_link = self.driver.find_element_by_link_text("Hjem")
+        home_link.click()
+        assert self.driver.title == "Tromsøstudentenes Dataforening | Hjem"
 
+  #  def test_styret(self):
+
+
+   # def test_about(self):
+
+
+#    def test_contact(self):
 
